b700b8ff130e0a767f8ba1202f5ddb00eca264f3
P14BAndi2003 <106509349+P14BAndi2003@users.noreply.github.com>
2023-05-07 15:05:02 +0300

    fze

--- resources/views/articles/home.blade.php
+++ resources/views/articles/home.blade.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-		<meta name="description" content="Magz is a HTML5 & CSS3 magazine template is based on Bootstrap 3.">
-		<!-- Shareable -->
-		<title>Magz &mdash; Responsive HTML5 &amp; CSS3 Magazine Template</title>
+<head>
+<meta charset="utf-8">
+	<title>Tous les derniers articles de l'IA sur IA_Info</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<meta name="description" content="Un site spécialisé sur les informations sur l'IA | IA_Info est en voie de développement pour donner le plus d'expérience possible aux amoureux de l'informatique surtout de l'IA">  
+
+	
 
  	<!-- CSS
    ================================================== -->
