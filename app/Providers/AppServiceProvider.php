df326c774d71fdbace6335a4dd18efe3d2f67d99
P14BAndi2003 <106509349+P14BAndi2003@users.noreply.github.com>
2023-05-08 18:44:27 +0300

    uyt

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
-      /*  if(request()->server('HTTP_ACCEPT_ENCODING') && strpos(request()->server('HTTP_ACCEPT_ENCODING'),'gzip')!==false)
+       /* if(request()->server('HTTP_ACCEPT_ENCODING') && strpos(request()->server('HTTP_ACCEPT_ENCODING'),'gzip')!==false)
         {
             ob_start("ob_gzhandler");
         }*/
