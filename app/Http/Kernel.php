0c60e0ec87e3aec083099b24f7bac3e79797e06f
P14BAndi2003 <106509349+P14BAndi2003@users.noreply.github.com>
2023-05-08 19:38:48 +0300

    vn

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -18,8 +18,9 @@ class Kernel extends HttpKernel
         // \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
         \Illuminate\Http\Middleware\HandleCors::class,
-        \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \App\Http\Middleware\CacheMiddleware::class,
+        \App\Http\Middleware\GzipMiddleware::class,
+        \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
@@ -41,7 +42,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\GzipMiddleware::class,
+            
         ],
 
         'api' => [
