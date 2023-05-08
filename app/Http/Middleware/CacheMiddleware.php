e9fd5d530ee1e804507722bea13ec0f17bc41fe4
P14BAndi2003 <106509349+P14BAndi2003@users.noreply.github.com>
2023-05-08 19:25:42 +0300

    jktrfj

--- app/Http/Middleware/CacheMiddleware.php
+++ app/Http/Middleware/CacheMiddleware.php
@@ -17,14 +17,11 @@ class CacheMiddleware
     {
         $response = $next($request);
 
-        
-        
-               // Configure cache headers for static files
-        if (in_array($response->getStatusCode(), [200, 201, 203, 204, 206, 300, 301, 302, 304, 307, 308])) {
-            $response->header('Cache-Control', 'public, max-age=31536000, immutable');
-        }
 
-        
+    // Configure cache headers for static files
+        if (in_array($response->getStatusCode(), [200, 304])) {
+            $response->header('Cache-Control', 'public, max-age=31538000, immutable');
+        }
 
         return $response;
     }
