df91e615e9340aa30cd3d016dcc6e1e7c98d1c63
P14BAndi2003 <106509349+P14BAndi2003@users.noreply.github.com>
2023-05-08 11:59:14 +0300

    rfrf

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -38,6 +38,8 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\CacheMiddleware::class,
+            \App\Http\Middleware\GzipMiddleware::class,
         ],
 
         'api' => [
