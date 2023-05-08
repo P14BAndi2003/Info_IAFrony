2d7c17f11bca195a05a093f26a51682030f33d8f
P14BAndi2003 <106509349+P14BAndi2003@users.noreply.github.com>
2023-05-08 13:34:37 +0300

    gjf

--- resources/views/articles/home.blade.php
+++ resources/views/articles/home.blade.php
@@ -10,9 +10,7 @@
 		<!-- Shareable -->
 		<title>Tous les derniers articles de l'IA sur IA_Info</title>
 		<!-- Bootstrap -->
-		<link rel="stylesheet" href="assets/scripts/bootstrap/bootstrap.min.css">
 		<!-- IonIcons -->
-		<link rel="stylesheet" href="assets/scripts/ionicons/css/ionicons.min.css">
 		<!-- Toast -->
 		<link rel="stylesheet" href="assets/scripts/toast/jquery.toast.min.css">
 		<!-- OwlCarousel -->
@@ -20,9 +18,7 @@
 		<link rel="stylesheet" href="assets/scripts/owlcarousel/dist/assets/owl.theme.default.min.css">
 		<!-- Magnific Popup -->
 		<link rel="stylesheet" href="assets/scripts/magnific-popup/dist/magnific-popup.css">
-		<link rel="stylesheet" href="assets/scripts/sweetalert/dist/sweetalert.css">
 		<!-- Custom style -->
-		<link rel="stylesheet" href="assets/css/style.css">
 		<link rel="stylesheet" href="assets/css/skins/all.css">
 		<link rel="stylesheet" href="assets/css/demo.css">
 	</head>
